0ad2f41655885175d4ca45bcb8b78d0e57fc1dd6
RatulGhosh <ratulghoshr@gmail.com>
2016-08-27 00:55:14 +0530

    resubmitted

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,22 @@
 import media
 import fresh_tomatoes
-movie1 = media.Movie('Saving Private Ryan','https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg','https://www.youtube.com/watch?v=zwhP5b4tD6g')
-movie2 = media.Movie('The Bucket List','http://briansrunningadventures.com/wp-content/uploads/2014/12/bucketlist.jpg','https://www.youtube.com/watch?v=UvdTpywTmQg')
-movie3 = media.Movie('The Shawshank Redemption','http://www.deckchaircinema.com/wp-content/uploads/2016/07/the-shawshank-redemption.jpg','https://www.youtube.com/watch?v=6hB3S9bIaco')
 
+# Create instanes of Movie class
+movie1 = media.Movie('Saving Private Ryan',
+                     'http://goo.gl/EPaZGD',
+                     'https://www.youtube.com/watch?v=zwhP5b4tD6g')
+movie2 = media.Movie('The Bucket List',
+                     'http://goo.gl/aXAl8E',
+                     'https://www.youtube.com/watch?v=UvdTpywTmQg')
+movie3 = media.Movie('The Shawshank Redemption',
+                     'http://goo.gl/EbgjBk',
+                     'https://www.youtube.com/watch?v=6hB3S9bIaco')
+
+# Appending each object of Movie class in a list
 movie = []
 movie.append(movie1)
 movie.append(movie2)
 movie.append(movie3)
 
+# Calling the function that renders the html page
 fresh_tomatoes.open_movies_page(movie)
--- media.py
+++ media.py
@@ -1,8 +1,8 @@
-class Movie :
-	
-	def __init__(self,title,poster_image_url,trailer_youtube_url):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-	
-	
+class Movie:
+    """Initialise the variable of the Movie object"""
+
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
